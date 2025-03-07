47dc4fe31832aa833b451d742a74ea213c23a5b7
vicevalds <vicentevaldesfigueroa@protonmail.com>
2025-03-07 13:13:01 -0300

    add(apps) ublock origin

--- apps.sh
+++ apps.sh
@@ -11,6 +11,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 # Others
+firefox https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/
 firefox https://discord.com/api/download?platform=linux&format=deb
 firefox https://obsidian.md/download
 firefox https://telegram.org/dl/desktop/linux
