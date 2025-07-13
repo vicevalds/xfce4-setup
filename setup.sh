c2252edc38050e5ebb873e3ee47cd9bd9be4d8d5
vicevalds <vicentevaldesfigueroa@protonmail.com>
2025-07-12 22:20:08 -0400

    check root

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
-BACKGROUND="monet.png"
+local BACKGROUND="monet.png"
+
+if [[ $(whoami) == "root" ]]; then
+  echo "[!] Run this script as unprivileged user (no sudo)"
+  exit 1
+fi
+
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y xfce4 xfce4-panel-profiles xfce4-power-manager
@@ -20,6 +26,7 @@ cp .bashrc ~/.
 # Set xfce4 enviroment
 xfce4-panel-profiles load xfce4-custom.tar.bz2
 xfconf-query -c xfce4-desktop -p $(xfconf-query -c xfce4-desktop -l | grep "workspace0/last-image") -s /usr/share/images/desktop-base/${BACKGROUND}
+
 # Custom lightdm
 bash lightdm.sh
 
